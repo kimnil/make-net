5383cda5bd8d3829e83be604cb2bbbe7c04485b1
Kim Nilsson <droogie.kim@gmail.com>
2016-04-20 20:44:19 +0200

    No docker interaction yet.

---
+++ bin/make-net.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const cp = require('child_process');
+
+const stdio = process.stdin,
+      stdout = process.stdout,
+      containers = [];
+
+
+if (process.argv.length < 3) {
+    stdout.write("You did not supply a net specification file! How am I suppouse to know what network you want me to make!?\n");
+    showUsage();
+    process.exit();
+}
+
+const args = process.argv.slice(2);
+const netSpecFile = args[0];
+
+const netSpec = parseFile(netSpecFile);
+const nodes = readNodeSpecs(netSpec);
+spawnContainers(nodes);
+
+logNodes(nodes);
+
+function showUsage() {
+    stdout.write("\n");
+    stdout.write("Usage: \n");
+    stdout.write("make-net my-net-specification.json \n");
+}
+
+function parseFile(pathToNetSpec) {
+    try {
+        const data = fs.readFileSync(pathToNetSpec, 'utf8');
+    } catch(err) {
+        console.log("Could not open " + pathToNetSpec);
+        console.log("Are you sure it exists and is readable for me? \nI was looking for it here: " + process.cwd());
+        process.exit();
+    }
+
+    try {
+        return JSON.parse(data);
+    } catch (err) {
+        console.log("I found " + pathToNetSpec + " but I didn't like it! I could not parse it. Invalid JSON?");
+    }
+}
+
+function readNodeSpecs(netSpec) {
+    const specs = {};
+    for (nodeName in netSpec) {
+       specs[nodeName] = specs[nodeName] || parseFile(netSpec[nodeName]); 
+    }
+    return specs;
+}
+
+function spawnContainers(nodes) {
+    for(node in nodes) {
+       nodes[node].hash = spawnContainer(nodes[node].container);
+       nodes[node].ip = getIpFromHash(nodes[node].hash);
+    }
+}
+
+function getIpFromHash(hash) {
+    return "ip";
+}
+
+function spawnContainer(container) {
+    console.log("docker exec something.... " + container);
+    return "#123";
+}
+
+function logNodes(nodes) {
+    Object.keys(nodes).forEach( (node) => {
+        console.log(node + "\t" + nodes[node].ip);
+    });
+}
